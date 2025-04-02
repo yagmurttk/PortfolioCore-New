6f7531baa1938667b3d5325e7f1520da0608f03a
Yağmur K. <yagmurrkeserr1@gmail.com>
2025-04-02 21:58:47 +0300

    Lesson 2

---
+++ PortfolioCore/Controllers/AdminLayoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCore.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+    }
+}
