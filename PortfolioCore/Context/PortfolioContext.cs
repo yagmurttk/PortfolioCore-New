6eafd93c272181babda57bf0664296f0c7fa6f54
Yağmur K. <yagmurrkeserr1@gmail.com>
2025-04-08 22:03:36 +0300

    Lesson 3

---
+++ PortfolioCore/Context/PortfolioContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioCore.Entities;
+
+namespace PortfolioCore.Context
+{
+    public class PortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-GI3R06F\\SQLEXPRESS; initial Catalog= PortfoliCoreDb; integrated security=true");
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
---
+++ PortfolioCore/Controllers/ExperienceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioCore.Context;
+using PortfolioCore.Entities;
+
+namespace PortfolioCore.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        PortfolioContext context = new PortfolioContext();
+        public IActionResult ExperienceList()
+        {
+            var values = context.Experiences.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+    }
+}
