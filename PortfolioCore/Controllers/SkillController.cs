6c760fb3b4dc2c8fc2b5b158151bc716ce8420e2
Yağmur K. <yagmurrkeserr1@gmail.com>
2025-04-15 22:27:22 +0300

    Lesson 5

--- PortfolioCore/Controllers/SkillController.cs
+++ PortfolioCore/Controllers/SkillController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PortfolioCore.Context;
+using PortfolioCore.Entities;
 
 namespace PortfolioCore.Controllers
 {
@@ -8,7 +9,44 @@ namespace PortfolioCore.Controllers
         PortfolioContext context = new PortfolioContext();
         public IActionResult SkillList()
         {
+            var values = context.Skills.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateSkill()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSkill(Skill skill)
+        {
+            context.Skills.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+        [HttpGet]
+        public IActionResult UpdateSkill(int id)
+        {
+            var value = context.Skills.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateSkill(Skill skill)
+        {
+            context.Skills.Update(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        public IActionResult DeleteSkill(int id)
+        {
+            var value = context.Skills.Find(id);
+            context.Skills.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
         }
     }
 }
